ae126d8d75671cf77908a18adc88dccbebd1b8f2
Sarthak Paliwal <159693158+Sarthak-Paliwal@users.noreply.github.com>
2025-08-08 12:14:41 +0530

    Url Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());//parsing to json
 app.use(express.urlencoded({extended:true }));
 app.use(cookieParser());
 const corsOptions={
-    origin:"http://localhost:5173",
+    origin:"https://jobportal-x8p5.onrender.com",
     credentials:true,
 }
 app.use(cors(corsOptions));
